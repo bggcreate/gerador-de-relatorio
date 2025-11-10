8f5e5be2dc373065ef2f10c80a6d75c532dcb0e4
Seu Nome <alexcoejunior@gmail.com>
2025-11-10 16:49:39 -0300

    intelbras

--- bin/DemoSrc/playsdkdemo/CharactorTansfer.h
+++
@@ -1,8 +0,0 @@
-#ifndef CHARACTOR_TANSFER
-#define CHARACTOR_TANSFER
-
-#include <string>
-
-std::string UnicodeToAnsi (const std::wstring& strUnicode);//²ÎÊýÊÇwstring
-std::wstring AnsiToUnicode(const std::string& strAnsi);
-#endif
